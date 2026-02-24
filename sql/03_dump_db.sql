d9f2062afddf0c2176838592d917ab9de495b3e7
FuturePhisicist <1kruglovlev1@gmail.com>
2026-02-24 20:36:31 +0300

    Fixed gradlew dbDump

---
+++ sql/03_dump_db.sql
@@ -0,0 +1,17 @@
+SET search_path = hr;
+
+\echo '=== Departments ==='
+TABLE Departments;
+
+\echo '=== Positions ==='
+TABLE Positions;
+
+\echo '=== DepartmentPositions ==='
+TABLE DepartmentPositions;
+
+\echo '=== Employees ==='
+TABLE Employees;
+
+\echo '=== Assignments ==='
+TABLE Assignments;
+
